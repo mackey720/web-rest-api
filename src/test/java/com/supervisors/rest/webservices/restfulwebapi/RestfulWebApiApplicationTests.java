4497954fa45fb7235146ab81ac0b4d614d7f998e
Alicia McPherson <mackey720@gmail.com>
2021-09-13 18:00:56 -0400

    commit spring files

---
+++ src/test/java/com/supervisors/rest/webservices/restfulwebapi/RestfulWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supervisors.rest.webservices.restfulwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
