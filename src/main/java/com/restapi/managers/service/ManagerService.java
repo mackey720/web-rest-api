33c527f6dfcf5cb4ffab2b52a15660bdfcdd2ea2
Alicia McPherson <mackey720@gmail.com>
2021-09-14 19:48:06 -0400

    push updates

--- src/main/java/com/restapi/managers/service/ManagerService.java
+++ src/main/java/com/restapi/managers/service/ManagerService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Component;
 
 import com.restapi.managers.Managers;
@@ -23,9 +24,12 @@ public class ManagerService {
 		
 		for (Managers manager : managers)
 		{
-			sortedManagers.add("{" + manager.getJurisdiction() + "}" 
-					+ " - " + "{" + manager.getLastName() + "}" 
-					+ ", " + "{" + manager.getFirstName() + "}");
+			if(!NumberUtils.isDigits(manager.getJurisdiction()))
+			{
+				sortedManagers.add("{" + manager.getJurisdiction() + "}" 
+						+ " - " + "{" + manager.getLastName() + "}" 
+						+ ", " + "{" + manager.getFirstName() + "}");
+			}
 		}
 		
 		return sortedManagers;
---
+++ src/test/java/com/restapi/managers/service/ManagerServiceTest.java
@@ -0,0 +1,14 @@
+package com.restapi.managers.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ManagerServiceTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
